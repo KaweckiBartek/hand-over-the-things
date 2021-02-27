de8b75e1225b3a5beade9b0233d243fa957e4077
KaweckiBartek <67326141+KaweckiBartek@users.noreply.github.com>
2021-02-27 13:09:25 +0100

    home layout ready

--- components/home/contactUs/index.tsx
+++ components/home/contactUs/index.tsx
@@ -13,50 +13,56 @@ const ContactUs = () => {
   }
   return (
     <div className="contactUs__background">
-      <div className="contactUs">
-        <HeadingItem firstLine="Skontaktuj się z nami" secondLine="" />
-        <form
-          onSubmit={handleSubmit}
-          className="contactUs__form">
+      <div className="contactUs__background-shadow">
+        <div className="contactUs">
+          <HeadingItem firstLine="Skontaktuj się z nami" secondLine="" />
+          <form
+            onSubmit={handleSubmit}
+            className="contactUs__form">
 
-          <div className="inputs__wrapper">
+            <div className="inputs__wrapper">
+              <label
+                className="form-label"
+                htmlFor="name">Wpisz swoje imię
+                <input
+                  className="form__input"
+                  id="input-name"
+                  value={name}
+                  type="text"
+                  placeholder="Krzysztof"
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </label>
+
+              <label
+                className="form-label"
+                htmlFor="input-email">Wpisz swój email
+                <input
+                  className="form__input"
+                  id="input-email"
+                  value={email}
+                  placeholder="abc@xyz.pl"
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="text"
+                />
+              </label>
+            </div>
+
             <label
               className="form-label"
-              htmlFor="name">Wpisz swoje imię
-          <input
-                className="form__input"
-                id="input-name"
-                value={name}
-                type="text"
-                onChange={(e) => setName(e.target.value)}
+              htmlFor="input-textarea">Wpisz swoją wiadomość
+              <textarea
+                className="form__textarea"
+                id="input-textarea"
+                value={message}
+                placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                onChange={(e) => setMessage(e.target.value)}
               />
             </label>
 
-            <label
-              className="form-label"
-              htmlFor="input-email">Wpisz swój email
-          <input
-                className="form__input"
-                id="input-email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                type="text"
-              />
-            </label>
-          </div>
-
-          <label
-            className="form-label"
-            htmlFor="input-textarea">Wpisz swoją wiadomość</label>
-          <textarea
-            className="form__textarea"
-            id="input-textarea"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          />
-
-          <MainButton text="Wyślij" size="small" />
-        </form>
+            <MainButton text="Wyślij" size="small" />
+          </form>
+        </div>
       </div>
     </div>
   )
